1c827428a3149e60756ec37397e2a685114c35bf
yousefmarey12 <yousef.marey@ucalgary.ca>
2025-11-07 18:42:34 +0300

    Change port

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ server.post('/login', (req, res) => {
             console.log(e)
         })
 })
-
-server.listen(5000, '0.0.0.0', () => {
-    console.log("Server is running on 8080")
+let port = process.env.PORT | 3000
+server.listen(port, '0.0.0.0', () => {
+    console.log("Server is running on " + port)
 })
